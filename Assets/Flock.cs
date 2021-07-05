fe858639541b8e0858bb07936a74d8b290869754
Leinad-Dev <55466788+Leinad-Dev@users.noreply.github.com>
2021-07-04 21:09:28 -0700

    Initial commit

---
+++ Assets/Flock.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockManager managerRef;
+    float speed;
+
+    private void Start()
+    {
+        speed = UnityEngine.Random.Range(managerRef.minSpeed, managerRef.maxSpeed);
+    }
+    private void Update()
+    {
+        ApplyRules();
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+        private void ApplyRules()
+        {
+            GameObject[] fishHolder;
+            fishHolder = managerRef.allFish;
+            Vector3 vGrpCenter = Vector3.zero;  //Point to center of group
+            Vector3 vAvoidance = Vector3.zero; //Point away from particular neighbor
+            float avgGrpSpeed = 0.01f;
+            float neighborDistance;
+            int grpSize = 0;
+
+            foreach (GameObject g in fishHolder)
+            {
+                if (g != this.gameObject) //no need to calculate distance between the same fish
+                {
+                    neighborDistance = Vector3.Distance(g.transform.position, this.transform.position);
+                    if(neighborDistance <= managerRef.neighborDistanceRequirement) // within range to be a neighbor.
+                    {
+                        vGrpCenter += g.transform.position;
+                        grpSize++;
+
+                        if(neighborDistance < managerRef.fishClipRange) //nbr fish too close to this fish, add opposite vector to avgAvoidance
+                        {
+                        //VectorAwayFromNeighborFish = CurrePosition - PositionWeWantToGetAwayFrom
+                        //You can find the vector for A to B by subtracting A from B (posB - posA)
+                        Vector3 vectorAway = this.transform.position - g.transform.position;
+                        vAvoidance = vAvoidance + vectorAway;
+                        Debug.DrawRay(transform.position, vectorAway, Color.blue);
+                        }
+                        Flock anotherFlock = g.GetComponent<Flock>();
+                        avgGrpSpeed = avgGrpSpeed + anotherFlock.speed;
+                    }
+                }
+            }
+
+            if (grpSize > 0) //check to see if we have a group
+            {
+                vGrpCenter = vGrpCenter / grpSize;
+                speed = avgGrpSpeed / grpSize;
+
+                Vector3 direction = (vGrpCenter + vAvoidance) - transform.position;
+                if (direction != Vector3.zero)
+                {
+                    transform.rotation = Quaternion.Slerp(transform.rotation,
+                        Quaternion.LookRotation(direction), managerRef.rotationSpeed * Time.deltaTime);
+                }
+            }
+
+
+
+
+
+
+
+        }
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector3.forward)*3);
+    }
+}
---
+++ Assets/FlockManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    [SerializeField] GameObject agentPrefab;
+    [Range(10, 100)] public int numFish;
+    public GameObject[] allFish;
+    [SerializeField] Vector3 spawnAreaLimit = new Vector3(5, 5, 5);
+
+    [Header("Fish Settings")]
+    [Range(0.1f, 5.0f)]
+    public float minSpeed;
+    [Range(0.1f, 5.0f)]
+    public float maxSpeed;
+    [Range(1.0f, 200.0f)]
+    public float neighborDistanceRequirement;
+    [Range(1.0f, 5.0f)]
+    public float rotationSpeed;
+    [Range(1.0f, 20.0f)]
+    public float fishClipRange;
+
+    private void Start()
+    {
+        allFish = new GameObject[numFish]; //initialize array
+        for(int i=0; i<numFish; i++) //initilize all agents positions in array
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-spawnAreaLimit.x, spawnAreaLimit.x),
+                                                                Random.Range(-spawnAreaLimit.y, spawnAreaLimit.y),
+                                                                Random.Range(-spawnAreaLimit.z, spawnAreaLimit.z));
+            allFish[i] = (GameObject)Instantiate(agentPrefab, pos, Quaternion.identity);
+            allFish[i].GetComponent<Flock>().managerRef = this;
+        }
+    }
+}
